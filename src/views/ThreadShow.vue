514f4fd5cfab2ef81bf6bcc78a17c2aca1eabdb1
Adam <adamorlowskipoland@icloud.com>
2018-10-14 14:47:47 +0200

    add new view, use router params as prop in component

---
+++ src/views/ThreadShow.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="col-large push-top">
+    <h1>{{ thread.title }}</h1>
+    <div class="post-list">
+      <div v-for="postId in thread.posts"
+           v-bind:key="postId"
+           class="post">
+        <div class="user-info">
+          <a href="#" class="user-name">{{ users[posts[postId].userId].name }}</a>
+          <a href="#">
+            <img :src="users[posts[postId].userId].avatar"
+                 alt="User's avatar"
+                 class="avatar-large">
+          </a>
+          <p class="desktop-only text-small">107 posts</p>
+        </div>
+        <div class="post-content">
+          <div>
+            {{ posts[postId].text }}
+          </div>
+        </div>
+        <div class="post-date text-faded">
+          {{ posts[postId].publishedAt }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import sourceData from '@/data/data.json';
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      thread: sourceData.threads[this.id],
+      posts: sourceData.posts,
+      users: sourceData.users,
+
+    };
+  },
+};
+</script>
