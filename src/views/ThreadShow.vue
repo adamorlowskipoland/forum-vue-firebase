6b672af2a309e672155eb2e9ad0233dc333c22ba
Adam <adamorlowskipoland@icloud.com>
2018-10-21 21:55:40 +0200

    add form to comment thread

--- src/views/ThreadShow.vue
+++ src/views/ThreadShow.vue
@@ -2,6 +2,19 @@
   <div class="col-large push-top">
     <h1>{{ thread.title }}</h1>
     <PostList :posts="posts"/>
+    <form @submit.prevent="addPost">
+      <div class="form-group">
+        <textarea name=""
+                  id=""
+                  cols="30"
+                  rows="10"
+                  class="form-input"
+                  v-model="newPostText"></textarea>
+      </div>
+      <div class="form-actions">
+        <button class="btn-blue">Submit post</button>
+      </div>
+    </form>
   </div>
 </template>
 <script>
@@ -21,6 +34,7 @@ export default {
   data() {
     return {
       thread: sourceData.threads[this.id],
+      newPostText: '',
 
     };
   },
@@ -28,6 +42,22 @@ export default {
     posts() {
       const postIds = Object.values(this.thread.posts);
       return Object.values(sourceData.posts).filter(post => postIds.includes(post['.key']));
+    },
+  },
+  methods: {
+    addPost() {
+      const postId = `newPost${Math.random()}`;
+      const post = {
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: '7uVPJS9GHoftN58Z2MXCYDqmNAh2',
+        '.key': postId,
+      };
+      this.$set(sourceData.posts, postId, post);
+      this.$set(this.thread.posts, postId, postId);
+      this.$set(sourceData.users[post.userId].posts, postId, postId);
+      this.newPostText = '';
     },
   },
 };
