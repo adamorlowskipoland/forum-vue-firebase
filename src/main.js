7f4f8201e58a60aba35c607f69d0508bcaeb8685
Adam <adamorlowskipoland@icloud.com>
2018-11-18 19:10:32 +0100

    add firebase

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import AppDate from '@/components/AppDate.vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
+const config = {
+  apiKey: 'yourAppApiKey',
+  authDomain: 'yourAuthDomain',
+  databaseURL: 'yourDatabaseURL',
+  projectId: 'yourProjectId',
+  storageBucket: 'yourStorageBucket',
+  messagingSenderId: 'yourSenderId',
+};
+firebase.initializeApp(config);
 
 // registering global component
 Vue.component('AppDate', AppDate);
