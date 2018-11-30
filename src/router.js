15879f9c067d3a4e2c199b2a76ffeef68e880b86
Adam Orlowski <adamorlowskipoland@icloud.com>
2018-11-30 17:00:52 +0100

    redirect user to destination after signing in

--- src/router.js
+++ src/router.js
@@ -130,7 +130,7 @@ router.beforeEach((to, from, next) => {
         if (user) {
           next();
         } else {
-          next({ name: 'SignIn' });
+          next({ name: 'SignIn', query: { redirectTo: to.path } });
         }
       } else if (to.matched.some(route => route.meta.requiresGuest)) {
         //  protect route
--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -49,13 +49,17 @@ export default {
         email: this.form.email,
         password: this.form.password,
       })
-        .then(() => this.$router.push('/'))
+        .then(() => this.successRedirect())
         .catch(error => alert(`💀 ${error.message}`));
     },
     SignInWithGoogle() {
       this.$store.dispatch('signInWithGoogle')
-        .then(() => this.$router.push('/'))
+        .then(() => this.successRedirect())
         .catch(error => alert(`💀 ${error.message}`));
+    },
+    successRedirect() {
+      const redirectTo = this.$route.query.redirectTo || { name: 'Home' };
+      this.$router.push(redirectTo);
     },
   },
   created() {
